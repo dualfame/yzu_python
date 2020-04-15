717a7f56b62ea4d74221db0b0523c2e635fa59e3
dualfame <63353299+dualfame@users.noreply.github.com>
2020-04-15 19:56:49 +0800

    Add files via upload

---
+++ case02.py
@@ -0,0 +1,11 @@
+num2 = 5 + True
+print(num2)
+
+#轉型態
+a = "100"
+b = "90.56789"
+print(a+b)  #文字
+print(int(a)+float(b))    #數值
+
+score = 90
+print("成績 = " + str(score))
---
+++ case03.py
@@ -0,0 +1,14 @@
+import random
+
+a = random.randint(1, 10)  # 1 <= a <= 10
+print(a)
+b = random.randrange(1, 10)  # 1 <= b < 10
+print(b)
+print(a, b)
+
+# 四星彩電選
+n1 = random.randint(0, 9)
+n2 = random.randint(0, 9)
+n3 = random.randint(0, 9)
+n4 = random.randint(0, 9)
+print(n1, n2, n3, n4)
