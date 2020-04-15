8eb2675b3d2b5ecdf129fd78a9c841cde6c8d931
dualfame <63353299+dualfame@users.noreply.github.com>
2020-04-15 20:20:00 +0800

    Add files via upload

--- case03.py
+++ case03.py
@@ -12,3 +12,15 @@ n2 = random.randint(0, 9)
 n3 = random.randint(0, 9)
 n4 = random.randint(0, 9)
 print(n1, n2, n3, n4)
+
+#樂透
+L1 = L2 = L3 = L4 = L5 = L6 = random.randint(1, 43) #用陣列Matrix
+print(L1, L2, L3, L4, L5, L6)
+
+#跳脫字元
+print("\\");print('abdce')
+print("A\nB\tC")
+
+x = 100 + 200 +\
+    5^2 - 9
+print(x)
