8c7842a1d22a7d1b1d68adda1184adf510a1b18b
dualfame <63353299+dualfame@users.noreply.github.com>
2020-04-15 20:33:00 +0800

    Add files via upload

---
+++ case04.py
@@ -0,0 +1,11 @@
+score=90
+print("score", score)
+print("score", score, sep=",")
+print("score", score, sep="/")
+
+print("a")
+print("b")
+
+print("a", end="")
+print("b", end="")
+print("c") #預設end="\n"
