54fc1d8f3dddf763306d084cd2f7dcf11a79a052
dualfame <63353299+dualfame@users.noreply.github.com>
2020-04-15 21:06:18 +0800

    Add files via upload

---
+++ case06.py
@@ -0,0 +1,8 @@
+Exchange1 = 30.295
+print('%08.5f' % Exchange1)
+Exchange2 = 38.52
+print('%08.5f' % Exchange2)
+Exchange3 = 0.2832
+print('%08.5f' % Exchange3)
+Exchange4 = 0.00228
+print('%08.5f' % Exchange4)
