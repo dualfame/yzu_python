39053f8e43c11aa0b7aaaffcf03e64eabed8a92c
dualfame <63353299+dualfame@users.noreply.github.com>
2020-04-15 20:58:40 +0800

    Add files via upload

--- case05.py
+++ case05.py
@@ -1,8 +1,9 @@
 symbol = "2330"
 name = "台積電"
 price = 286.5
-shares = 5000
+shares = int(input('股數='))
 
 print("2330 台積電成交價 286.5 買進 5000股 (5張) 成本 1,432,500")
-# %s text, %d int, %f floating
-print()
+# %s text, %d int, %.nf floating (n=位數)
+print("%s %s 成交價 %.1f 買進 %07d股 (%d張) 成本 %.2f" %
+      (symbol, name, price, shares, shares/1000, price*shares))
